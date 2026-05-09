524198ec43110023bcf1eda34d38b2978d2ffbc7
Oleksandr Kyrakivskyi <sashakira70@example.com>
2026-05-10 00:59:48 +0300

    WWW

---
+++ lab6/build/CMakeFiles/Lab6.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab6.dir/link.d"
+  "CMakeFiles/Lab6.dir/src/VideoProcessor.cpp.o"
+  "CMakeFiles/Lab6.dir/src/VideoProcessor.cpp.o.d"
+  "CMakeFiles/Lab6.dir/src/main.cpp.o"
+  "CMakeFiles/Lab6.dir/src/main.cpp.o.d"
+  "Lab6"
+  "Lab6.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
